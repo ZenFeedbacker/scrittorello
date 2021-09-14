fac1651a7af5059bc71f0b6e4e0d2298e7e87d5e
ZenFeedbacker <feoulas@gmail.com>
2021-09-14 12:09:18 +0300

    add version printing info

--- src/main/java/com/scritorrelo/Scrittorello.java
+++ src/main/java/com/scritorrelo/Scrittorello.java
@@ -1,14 +1,34 @@
 package com.scritorrelo;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.FileReader;
+import java.io.IOException;
+
+@Slf4j
 @SpringBootApplication
 public class Scrittorello {
 
 
     public static void main(String[] args) {
         SpringApplication.run(Scrittorello.class);
+        printInfo();
+    }
+
+    static void printInfo(){
+
+        try {
+            MavenXpp3Reader reader = new MavenXpp3Reader();
+            Model model = reader.read(new FileReader("pom.xml"));
+            log.info("Running version {}", model.getVersion());
+        } catch (IOException | XmlPullParserException e) {
+            e.printStackTrace();
+        }
     }
 }
 
